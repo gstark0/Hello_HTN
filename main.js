6890577724e01c11bbf7a7bcf842c1a3c1096692
gstark0 <gstark0@icloud.com>
2018-07-15 11:24:50 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,23 @@
+var fadingTime = 500;
+var currentSlide = 0
+
+function changeSlide() {
+	slides = $('.slide');
+	if(currentSlide == slides.length - 1) {
+		$(slides[currentSlide]).fadeOut(fadingTime);
+		$(slides[0]).delay(fadingTime+100).fadeIn(fadingTime);
+		currentSlide = 0;
+	} else {
+		$(slides[currentSlide]).fadeOut(fadingTime);
+		$(slides[currentSlide + 1]).delay(fadingTime+100).fadeIn(fadingTime);
+		currentSlide = currentSlide + 1;
+	}
+}
+
+$('body').keypress(function() {
+	changeSlide();
+});
+
+$('body').on('swipe', swipeHandler);
+
+tippy('a')
